50520bc59d2c42a52cb84ddf18a00f94cf19bd74
Vinicius Rodrigues da silva <vinicius.rodrigues@class-solutions.com.br>
2024-04-26 15:11:45 -0300

    cores de fundo aplicadas na lista de animais.

--- src/app/animal-item/animal-item.component.ts
+++ src/app/animal-item/animal-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, input } from '@angular/core';
 import { Animal } from '../models/animal';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-animal-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './animal-item.component.html',
   styleUrl: './animal-item.component.css'
 })
